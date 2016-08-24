cd1853ae7bdf5339b2492cb3bcbe0ed8b0030071
master <libaoming2@gmail.com>
2016-08-24 17:19:31 +0800

    complete finde me location use CoreLocation kit

--- findMyLocation/ViewController.swift
+++ findMyLocation/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      
+        let userLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longtitude = userLocation.coordinate.longitude
+        
+        let longDelta: CLLocationDegrees = 0.05
+        let latiDelta: CLLocationDegrees = 0.05
+        
+        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        let span = MKCoordinateSpan(latitudeDelta: latiDelta, longitudeDelta:longDelta)
+        
+        
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        map.setRegion(region, animated: true)
+        
+        let annotation: MKPointAnnotation = MKPointAnnotation()
+        
+        annotation.coordinate.latitude = latitude
+        annotation.coordinate.longitude = longtitude
+        
+        map.addAnnotation(annotation)
+        
+        
+        
+    }
 }
 
